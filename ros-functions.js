f908c155aa149b02af1c23a68114921a6553b216
cobyc15 <cmchiu515@gmail.com>
2023-04-23 15:26:51 -0400

    simple app working

---
+++ ros-functions.js
@@ -0,0 +1,8 @@
+
+
+// extracts the value of the selection made by user
+// sends corresponding move goal
+function sendgoal(element) {
+    var location = element.value;
+    console.log(location);
+}
